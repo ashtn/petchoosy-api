42c148d7785642322861776e101813d0b23ae28d
Ashtn <ashtonehorton@gmail.com>
2017-07-26 05:55:40 -0700

    created login route, and controller action

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,11 @@
+class UserController < ApplicationController
+  def login
+    user = User.find_by(email: params[:email])
+
+    if user
+      render status: :ok, json: {user_id: user.id}
+    else
+      render status: :error, json: 'Could Not Find Account, Please try again'
+    end
+  end
+end
